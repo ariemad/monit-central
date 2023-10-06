2627f752fe8c36c21849c29cc3e8d0d44728ba80
Daniel Bray <daniel.bray.dev@hotmail.com>
2023-10-06 11:24:14 +0100

    Update help

--- scripts/help.js
+++ scripts/help.js
@@ -4,13 +4,15 @@ const { vars } = require("./vars");
 const help = () => {
   let string = `Commands:
 
-help - Get help
+help - This text
 
 add -t [${vars.hostConnTypes.join(
     ", "
   )}] -ip [123.1.2.3] -u [admin] -path [/path/to/key] - Add a host connection
 
-clear [${vars.hostConnTypes.join(", ")}] - Clear all host connection 
+clear - Clear all host connections 
+
+start - Start monitoring all the hosts
     `;
 
   message(string, { all: true });
